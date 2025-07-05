53b24c6732206575f0413526f46766ca82bda84f
fazaram <fazaramanugraha@gmail.com>
2025-07-05 14:13:25 +0700

    update index porto

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,15 @@ module.exports = {
   content: ["./*.html"],
   theme: {
     extend: {
-      screens: {
-        sm: "480px",
-      },
       spacing: {
         big: "48rem",
       },
+    },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
     },
     fontFamily: {
       nunito: ["Nunito", "sans-serif"],
